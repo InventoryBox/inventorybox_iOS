d4fe98f0d1c1d82f2e4ad20f0008d08ae9e6204a
jaeyong <maxwodyd98@gmail.com>
2020-07-02 19:56:00 +0900

    재고 기록 탭 스토리보드 추가

---
+++ inventorybox_iOS/inventorybox_iOS/Sources/VCs/IvRecordVC.swift
@@ -0,0 +1,18 @@
+//
+//  IvRecordVC.swift
+//  inventorybox_iOS
+//
+//  Created by 이재용 on 2020/07/02.
+//  Copyright © 2020 jaeyong Lee. All rights reserved.
+//
+
+import UIKit
+
+class IvRecordVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
