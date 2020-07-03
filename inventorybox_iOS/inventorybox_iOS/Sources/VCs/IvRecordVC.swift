aaa43c1d455fce77502ed46705e1ab793c13c458
jaeyong <maxwodyd98@gmail.com>
2020-07-03 18:47:37 +0900

    재고 기록 7/3

--- inventorybox_iOS/inventorybox_iOS/Sources/VCs/IvRecordVC.swift
+++ inventorybox_iOS/inventorybox_iOS/Sources/VCs/IvRecordVC.swift
@@ -10,9 +10,35 @@ import UIKit
 
 class IvRecordVC: UIViewController {
     
+    @IBOutlet weak var categoryCollectionView: UICollectionView!
+    
+    let categoryArray: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
+}
+
+//MARK: - CollectionView
+extension IvRecordVC: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return categoryArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let categoryCell = collectionView.dequeueReusableCell(withReuseIdentifier: MusicCell.identifier, for: indexPath) as? MusicCell else { return UICollectionViewCell() }
+        }
+        musicCell.set(musicList[indexPath.row])
+        return musicCell
+        
+        
+    }
+    
+    
+}
+
+extension IvRecordVC: UICollectionViewDelegateFlowLayout, UICollectionViewDelegate {
+    
 }
