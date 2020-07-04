e6bdfeba6862e3f6ba40bb7b22c9d5122ec94bed
jaeyong <maxwodyd98@gmail.com>
2020-07-04 17:08:03 +0900

    디자인 파일 오류 해결을 위한 중간 커밋

---
+++ inventorybox_iOS/inventorybox_iOS/Sources/CategoryCell.swift
@@ -0,0 +1,13 @@
+//
+//  CategoryCell.swift
+//  inventorybox_iOS
+//
+//  Created by 이재용 on 2020/07/04.
+//  Copyright © 2020 jaeyong Lee. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UICollectionViewCell {
+    
+}
