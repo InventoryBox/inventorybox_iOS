1d7708ba6b9619d9d43a9b5388a00c54d3c6a2cd
Hwangho <63707317+Hwangho@users.noreply.github.com>
2020-08-18 18:48:37 +0900

    [create] 황호 HomeNewVC생성

---
+++ inventorybox_iOS/inventorybox_iOS/Sources/VCs/HomeNewVC.swift
@@ -0,0 +1,30 @@
+//
+//  HomeNewVC.swift
+//  inventorybox_iOS
+//
+//  Created by 송황호 on 2020/08/18.
+//  Copyright © 2020 jaeyong Lee. All rights reserved.
+//
+
+import UIKit
+
+class HomeNewVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
