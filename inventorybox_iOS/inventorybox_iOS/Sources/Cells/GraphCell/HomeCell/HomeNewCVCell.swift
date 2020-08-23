1f67d78f1b8f5a227dd6b8c181393a2b05795e71
Hwangho <63707317+Hwangho@users.noreply.github.com>
2020-08-23 23:02:28 +0900

    [update] 황호 HomeNew pagecontrol 생성

---
+++ inventorybox_iOS/inventorybox_iOS/Sources/Cells/GraphCell/HomeCell/HomeNewCVCell.swift
@@ -0,0 +1,35 @@
+//
+//  HomeNewTVCell.swift
+//  inventorybox_iOS
+//
+//  Created by 송황호 on 2020/08/21.
+//  Copyright © 2020 jaeyong Lee. All rights reserved.
+//
+
+import UIKit
+
+class HomeNewCVCell: UICollectionViewCell {
+    
+    @IBOutlet weak var viewRadious: UIView!
+    
+    let identifier : String = "HomeNewCVCell"
+    
+    @IBOutlet var leftStackImage: [UIImageView]! // left image stackview
+    @IBOutlet var leftStackview: [UILabel]! // left label stackview
+  
+    
+    
+    override func awakeFromNib() {
+        makeShadowUnderView()
+    }
+    
+    
+  // 그림자주는 코드
+   private func makeShadowUnderView() {
+       
+                viewRadious.layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
+                viewRadious.layer.shadowOpacity = 0.1
+                viewRadious.layer.shadowRadius = 8// 퍼지는 정도
+                viewRadious.layer.cornerRadius = 10
+   }
+}
