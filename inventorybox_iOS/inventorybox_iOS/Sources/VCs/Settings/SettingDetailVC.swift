423f1d585e5597907a5f983e1f99435e2f3604de
jaeyong <maxwodyd98@gmail.com>
2020-09-21 15:20:14 +0900

    [UPDATE] 재고창고 사용법 업데이트
    * 위 메뉴바 구현

--- inventorybox_iOS/inventorybox_iOS/Sources/VCs/Settings/SettingDetailVC.swift
+++ inventorybox_iOS/inventorybox_iOS/Sources/VCs/Settings/SettingDetailVC.swift
@@ -13,11 +13,13 @@ class SettingDetailVC: UIViewController {
     
     var navigationTitle: String?
     var whichInformation: Int?
-    
+    let menuArray: [String] = ["기본", "재고기록", "발주 알림", "재고량 추이", "재고교환"]
     @IBOutlet weak var userTermScrollView: UIScrollView!
     @IBOutlet weak var privacyPolicyScrollView: UIScrollView!
     @IBOutlet weak var locationTermScrollView: UIScrollView!
     @IBOutlet weak var inventoryBoxInstructionScrollView: UIScrollView!
+    @IBOutlet weak var menuCollectionView: UICollectionView!
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.navigationItem.title = navigationTitle
@@ -41,8 +43,32 @@ class SettingDetailVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        menuCollectionView.delegate = self
+        menuCollectionView.dataSource = self
+        
     }
     
+}
 
+extension SettingDetailVC: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//        <#code#>
+    }
+}
+
+extension SettingDetailVC: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return menuArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let menuCell = collectionView.dequeueReusableCell(withReuseIdentifier: MenuCell.identifier, for: indexPath) as? MenuCell else { return UICollectionViewCell() }
+        menuCell.set(title: menuArray[indexPath.row])
+        return menuCell
+    }
+    
 }
