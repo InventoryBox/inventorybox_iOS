9fc1160250cabf41daa6d14616456897c863d954
Hwangho <63707317+Hwangho@users.noreply.github.com>
2020-10-12 19:21:52 +0900

    [create]황호 검색view

--- inventorybox_iOS/inventorybox_iOS/Sources/Models/IvExchange/IvExchangeFavoriteInformation.swift
+++ inventorybox_iOS/inventorybox_iOS/Sources/Models/IvExchange/IvExchangeFavoriteInformation.swift
@@ -7,3 +7,54 @@
 //
 
 import Foundation
+
+// MARK: - IvHomeUserPostInformation
+struct IvExchangeFavoriteInformation: Codable {
+
+        // 서버 통신시
+//    let status: Int
+//    let success: Bool
+//    let message: String
+//    let data: ivExchangeFavoriteInfoClass?
+//}
+//
+//// MARK: - DataClass
+//struct ivExchangeFavoriteInfoClass: Codable {
+//    let ivexchangefavoriteInfo: [ivExchangeFavoriteInfo]
+//}
+//
+//// MARK: - PostInfo
+//struct ivExchangeFavoriteInfo: Codable {
+//    let postIdx: Int
+//    let productImg: String
+//    let productName: String
+//    let price: Int
+//    let expDate: String?
+//    let isSold: Int
+//    let uploadDate: String
+//    let likes: Int
+    
+    
+    var postIdx: Int
+    var productImg: String
+    var productName: String
+    var price: Int
+    var expDate: String?
+    var isSold: Int
+    var uploadDate: String
+    var likes: Int
+    
+    
+    init(postIdx: Int, productImg: String, productName: String, price: Int, expDate: String, isSold: Int, uploadDate: String, likes: Int ) {
+        
+        self.postIdx = postIdx
+        self.productImg = productImg
+        self.productName = productName
+        self.price = price
+        self.expDate = expDate
+        self.isSold = isSold
+        self.uploadDate = uploadDate
+        self.likes = likes
+ 
+    }
+}
